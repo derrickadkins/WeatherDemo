76273d2ff6c988bd9d062a83dba4dbf21c2e7ab3
Derrick Adkins <derrick.l.adkins@gmail.com>
2024-07-11 14:57:31 -0400

    Create RainIcon.tsx

---
+++ weather-frontend/src/components/RainIcon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RainIcon: React.FC = () => (
+  <svg className="inline-block w-5 h-5 mr-1" viewBox="0 -2 5 10" fill="#ADD8E6">
+    <title>Rain</title>
+    <path d="M4.7329.0217c-.1848-.059-.3855.0064-.4803.148L.2731 5.1191c-.0814.0922-.1501.1961-.196.3108-.2469.6009.1185 1.2697.8156 1.4943.6914.226 1.447-.0712 1.7-.6585L4.9662.4987l.0111-.0282c.073-.1807-.036-.379-.2444-.4488z"></path>
+  </svg>
+);
+
+export default RainIcon;
